332c9ff767812de68496258815723a4f8396ff98
soumik605 <67136855+soumik605@users.noreply.github.com>
2021-08-08 14:31:53 +0530

    Update index.js

--- CRUD_Model/index.js
+++ CRUD_Model/index.js
@@ -8,7 +8,7 @@ const app= express()
 const port = 8000
 
 
-const url = `mongodb://soumikmaity:soumik605@cluster0-shard-00-00.b2oo8.mongodb.net:27017,cluster0-shard-00-01.b2oo8.mongodb.net:27017,cluster0-shard-00-02.b2oo8.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-buj8su-shard-0&authSource=admin&retryWrites=true&w=majority`
+const url = `mongodb://soumikmaity:s*******5@cluster0-shard-00-00.b2oo8.mongodb.net:27017,cluster0-shard-00-01.b2oo8.mongodb.net:27017,cluster0-shard-00-02.b2oo8.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-buj8su-shard-0&authSource=admin&retryWrites=true&w=majority`
 
 
 app.use(bodyParser.json({extended:true}))
